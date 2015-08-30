cf700511f7cf814bd546a17c377d7424b0f1ab42
Marcella Merces <mgmerces@mit.edu>
2015-08-30 19:24:51 -0300

    criado enum com tokens

--- src/lexicalAnalyzer/LexicalAnalyzer.java
+++ src/lexicalAnalyzer/LexicalAnalyzer.java
@@ -1,5 +1,9 @@
 package lexicalAnalyzer;
 
 public class LexicalAnalyzer {
-
+	private static final String[] palavrasReservadas = {"array", "boolean", "break", "char", "continue", "do", "else", "false", "function", "if", "integer", "of", "string", "struct", "true", "type", "var", "while"};
+	
+	public LexicalAnalyzer() {
+		
+	}
 }
